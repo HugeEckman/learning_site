21cdff85fa257071351a43b1f4259edf28feca16
Дмитрий Евтушенко <user@user.com>
2023-12-13 17:05:30 +0300

    add class based view for courses

--- learning_app/views.py
+++ learning_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from models import Course
 
-# Create your views here.
+
+class CourseListView(ListView):
+    model = Course
+    template_name = 'learning_app/course_list.html' 
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'learning_app/course_detail.html' 
+
+
+class CourseCreateView(CreateView):
+    model = Course
+    fields = '__all__'
+    success_url = reverse_lazy('learning_app:course_list')
+
+
+class CourseUpdateView(UpdateView):
+    model = Course
+    fields = '__all__'
+    success_url = reverse_lazy('learning_app:course_list')
+
+
+class CourseDeleteView(DeleteView):
+    model = Course
+    success_url = reverse_lazy('learning_app:course_list')
+
