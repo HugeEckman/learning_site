7b8839b0efc92d4d33812171afb2199d82130a1f
Дмитрий Евтушенко <user@user.com>
2024-01-08 20:18:45 +0300

    change db structure

--- learning_app/models.py
+++ learning_app/models.py
@@ -4,29 +4,44 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=50)
+    goals = models.CharField(max_length=50)
+    # course = models.OneToOneField(Course, null=True, on_delete=models.SET_NULL)
+    teacher = models.OneToOneField(Teacher, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return self.title
+    
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
     category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
     start_date = models.DateField()
     finish_date = models.DateField()
-
-
-class Teacher(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
-
+    lessons = models.ForeignKey(Lesson, null=True, on_delete=models.SET_NULL)
+    
+    def __str__(self) -> str:
+        return self.name
+    
 
 class Catalog(models.Model):
-    course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
+    # course = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
+    courses = models.ForeignKey(Course, null=True, on_delete=models.SET_NULL)
 
-
-class Lesson(models.Model):
-    title = models.CharField(max_length=50)
-    goals = models.CharField(max_length=50)
-    course = models.OneToOneField(Course, null=True, on_delete=models.SET_NULL)
-    teacher = models.OneToOneField(Teacher, null=True, on_delete=models.SET_NULL)
 
 
 # class Schedule(models.Model):
