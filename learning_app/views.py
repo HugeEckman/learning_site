65c49cf318c0e8673aa7f7e1906fc77438c80ce7
Дмитрий Евтушенко <user@user.com>
2024-01-13 21:55:05 +0300

    add views for categories

--- learning_app/views.py
+++ learning_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Course
+from .models import Course, Category
 
 def index_view(request):
     return render(request, 'learning_app/index.html')
@@ -31,4 +31,33 @@ class CourseUpdateView(UpdateView):
 class CourseDeleteView(DeleteView):
     model = Course
     success_url = reverse_lazy('learning_app:course_list')
+
+# Categories
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'learning_app/category_list'
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'learning_app/category_detail.html' 
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = '__all__'
+    success_url = reverse_lazy('learning_app:category_list')
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = '__all__'
+    success_url = reverse_lazy('learning_app:category_list')
+
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy('learning_app:category_list')
+
+
 
